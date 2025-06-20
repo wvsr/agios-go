e39fcf80fb84a82bc6534e2fa0e9d27e0d6f6365
wvsr <email.wasimul@gmail.com>
2025-06-21 03:30:01 +0600

    fix: tavily resposne schema

--- internal/utils/llm/tavily.go
+++ internal/utils/llm/tavily.go
@@ -2,24 +2,47 @@ package llm
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/strrl/tavily-go/pkg/tavily"
 )
 
-func TavilySearch(apiKey, query string) ([]tavily.SearchResult, error) {
-	client := tavily.NewClient(apiKey)
+type SearchResponse struct {
+	Results []tavily.SearchResult `json:"results"`
+	Answer  string                `json:"answer"`
+	Images  []string              `json:"images"`
+}
+
+func TavilySearch(query string) (SearchResponse, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found or failed to load:", err)
+	}
+
+	tavilyApiKey := os.Getenv("TAVILY_API_KEY")
+	if tavilyApiKey == "" {
+		return SearchResponse{}, fmt.Errorf("TAVILY_API_KEY not set in environment")
+	}
+
+	client := tavily.NewClient(tavilyApiKey)
 
 	resp, err := client.SearchWithOptions(
 		context.Background(),
 		query,
 		tavily.WithMaxResults(10),
 	)
-
 	if err != nil {
-		log.Printf("tavily search failed: %v", err)
-		return []tavily.SearchResult{}, err
+		log.Printf("Tavily search failed: %v", err)
+		return SearchResponse{}, err
 	}
 
-	return resp.Results, nil
+	formattedResponse := SearchResponse{
+		Results: resp.Results,
+		Answer:  resp.Answer,
+		Images:  resp.Images,
+	}
+
+	return formattedResponse, nil
 }
