d8a638b1696bd256b119fc54fb55da9f8602f954
wvsr <email.wasimul@gmail.com>
2025-06-21 01:12:53 +0600

    chore: event and widget type constant

---
+++ internal/utils/constant/events.go
@@ -0,0 +1,10 @@
+package constant
+
+const (
+	EventStart          = "START"
+	EventEnd            = "END"
+	EventPlan           = "PLAN"
+	EventWebResults     = "WEB_RESULTS"
+	EventMarkdownAnswer = "MARKDOWN_ANSWER"
+	EventWidget         = "WIDGET"
+)
---
+++ internal/utils/constant/widget_type.go
@@ -0,0 +1,9 @@
+package constant
+
+const (
+	WidgetWeather      = "WEATHER_WIDGET"
+	WidgetCrypto       = "CRYPTO_WIDGET"
+	WidgetNearbyPlaces = "NEARBY_PLACES_WIDGET"
+	WidgetYTSummary    = "YT_SUMMARY_WIDGET"
+	WidgetSynthResults = "SYNTHESIZER_RESULTS"
+)
